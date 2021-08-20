155b62e6ee46e308c0b4f1b71e7dfc52a3acd679
Archit Yadav <archityadav97@gmail.com>
2021-08-21 00:14:05 +0530

    Add contour detection

--- docscanner.py
+++ docscanner.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-from utils import image_grid, width, height, draw_text
+from utils import image_grid, width, height, draw_text, biggest_contour, draw_rectangle, reorder
 
 PATH = "./img/tnk_art.jpg"
 
@@ -14,23 +14,35 @@ img = cv2.resize(img, (width, height))
 
 # Convert to grayscale
 img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-img = draw_text(img, "Original")
 
 # Add Gaussian blur
 img_blur = cv2.GaussianBlur(img_gray, (5, 5), 1)
-img_gray = draw_text(img_gray, "Grayscale")
 
 # Add thresholding
-img_threshold = cv2.Canny(img_blur, 100, 200)  # APPLY CANNY BLUR
-img_blur = draw_text(img_blur, "Gaussian Blur")
+img_threshold = cv2.Canny(img_blur, 100, 200)
+
+# Find all the contours
+img_contours = img.copy()
+imgBigContour = img.copy()
+contours, hierarchy = cv2.findContours(img_threshold, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cv2.drawContours(image=img_contours, contours=contours, contourIdx=-1, color=(0, 255, 0), thickness=10)
+
+# Find the biggest contour
+biggest, maxArea = biggest_contour(contours)
+biggest = reorder(biggest)
+cv2.drawContours(image=imgBigContour, contours=biggest, contourIdx=-1, color=(0, 255, 0), thickness=20)
+imgBigContour = draw_rectangle(imgBigContour, biggest, 2)
 
 blank_img = np.zeros((height, width, 3), dtype=np.uint8)
-img_threshold = draw_text(img_threshold, "Threashold")
+image_list = [img, img_gray, img_blur, img_threshold, img_contours, imgBigContour, blank_img, blank_img]
 
-image_list = [img, img_gray, img_blur, img_threshold, blank_img, blank_img, blank_img, blank_img]
+# Add labels to each image
+img = draw_text(img, "Original")
+img_gray = draw_text(img_gray, "Grayscale")
+img_blur = draw_text(img_blur, "Gaussian Blur", pos=(int(width / 4), 50))
+img_threshold = draw_text(img_threshold, "Canny Edge", pos=(int(width / 4), 50))
+img_contours = draw_text(img_contours, "Contours")
+imgBigContour = draw_text(imgBigContour, "Largest contour", pos=(int(width / 5), 50))
 
+# Combine the images into a grid
 image_grid = np.asarray(image_grid(image_list, width, height))
-
-cv2.imshow('Document Scan', image_grid)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 import cv2
 import PIL.Image as Image
-import os
+import numpy as np
 
 IMAGES_PATH = "./img/"
 
-IMAGE_ROW = 2
-IMAGE_COLUMN = 4
+ROWS = 2
+COLUMNS = 4
 IMAGE_SAVE_PATH = './img/final.png'
 
 height = 1280
@@ -26,12 +26,12 @@ def image_grid(image_names, IMAGE_SIZE_C, IMAGE_SIZE_R):
     """
 
     # Create a new canvas on which all the images would be placed
-    to_image = Image.new('RGBA', (IMAGE_COLUMN * IMAGE_SIZE_C, IMAGE_ROW * IMAGE_SIZE_R))
+    to_image = Image.new('RGBA', (COLUMNS * IMAGE_SIZE_C, ROWS * IMAGE_SIZE_R))
 
     # Loop through all pictures, paste each picture
-    for y in range(0, IMAGE_ROW):
-        for x in range(0, IMAGE_COLUMN):
-            from_image = image_names[y * IMAGE_COLUMN + x]
+    for y in range(0, ROWS):
+        for x in range(0, COLUMNS):
+            from_image = image_names[y * COLUMNS + x]
             to_image.paste(Image.fromarray(from_image), (x * IMAGE_SIZE_C, y * IMAGE_SIZE_R))
 
     to_image.save("./img/final.png")
@@ -40,12 +40,28 @@ def image_grid(image_names, IMAGE_SIZE_C, IMAGE_SIZE_R):
 
 def draw_text(img, text,
               font=cv2.FONT_HERSHEY_SIMPLEX,
-              pos=(int(width / 3), 100),
+              pos=(int(width / 3), 50),
               font_scale=3,
               font_thickness=8,
               text_color=(0, 0, 0),
               text_color_bg=(255, 255, 255)
               ):
+
+    """
+    Function to put overlay text on an image, with various tweaks possible.
+    Taken from https://stackoverflow.com/a/65146731/6475377 and made few modifications.
+
+    :param img: The base image to put text on
+    :param text: The text string
+    :param font: Font to use for text
+    :param pos: Starting position of the text (x, y)
+    :param font_scale: Size of text
+    :param font_thickness: Thickness of text
+    :param text_color: Colour of text
+    :param text_color_bg: Background colour of rectangle
+
+    :return: The modified image
+    """
     padding = 10
     x, y = pos
     text_size, _ = cv2.getTextSize(text, font, font_scale, font_thickness)
@@ -54,3 +70,77 @@ def draw_text(img, text,
     cv2.putText(img, text, (x, y + text_h + font_scale - 1), font, font_scale, text_color, font_thickness)
 
     return img
+
+
+def biggest_contour(contours):
+    biggest = np.array([])
+    max_area = 0
+    for i in contours:
+        # Get the area and perimeter of contour
+        area = cv2.contourArea(i)
+        peri = cv2.arcLength(i, True)
+
+        # Approximate the (closed) curve and get its points
+        approx = cv2.approxPolyDP(i, 0.02 * peri, True)
+
+        # If current contour is bigger than previous one, AND it is a rectangle
+        if area > max_area and len(approx) == 4:
+            biggest = approx
+            max_area = area
+
+    # Squeeze to 2D matrix: (4, 1, 2) -> (4, 2)
+    # biggest = np.squeeze(biggest, axis=1)
+
+    return biggest, max_area
+
+
+def draw_rectangle(img, biggest, thickness):
+    """
+    Draw a rectangle using 4 individual lines.
+
+    :param img: The base image on which to draw
+    :param biggest: The 4 points for the rectangle
+    :param thickness: Thickness of the 4 lines
+
+    :return: The modified image
+    """
+    # Squeeze to 2D matrix: (4, 1, 2) -> (4, 2)
+    biggest = np.squeeze(biggest, axis=1)
+
+    cv2.line(img, (biggest[0][0], biggest[0][1]), (biggest[1][0], biggest[1][1]), (0, 255, 0), thickness)
+    cv2.line(img, (biggest[0][0], biggest[0][1]), (biggest[2][0], biggest[2][1]), (0, 255, 0), thickness)
+    cv2.line(img, (biggest[3][0], biggest[3][1]), (biggest[2][0], biggest[2][1]), (0, 255, 0), thickness)
+    cv2.line(img, (biggest[3][0], biggest[3][1]), (biggest[1][0], biggest[1][1]), (0, 255, 0), thickness)
+
+    return img
+
+
+def reorder(points):
+    """
+    Reorder the points based on following notation:
+    (x_min, y_min), (x_max, y_min), (x_min, y_max), (x_max, y_max)
+
+    :param points: The 4 points to reorder
+    :return: The reordered 4 points -> dimension: (4, 1, 2)
+    """
+    # Squeeze to 2D matrix: (4, 1, 2) -> (4, 2)
+    points = np.squeeze(points, axis=1)
+
+    points_new = np.zeros((4, 1, 2), dtype=np.int32)
+
+    # Add the x and y values of all 4 points
+    add = points.sum(1)
+
+    # First point, with smallest x and y coordinates
+    points_new[0] = points[np.argmin(add)]
+
+    # Last point, with largest x and y coordinates
+    points_new[3] = points[np.argmax(add)]
+
+    # Subtract the x and y values of all 4 points
+    diff = np.diff(points, axis=1)
+
+    points_new[1] = points[np.argmin(diff)]
+    points_new[2] = points[np.argmax(diff)]
+
+    return points_new
